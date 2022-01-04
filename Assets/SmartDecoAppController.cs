8c4db37e81a22c6de4ad1b5a68b37c0e47d2421e
Ted Barnett <mail@tedbarnett.com>
2022-01-03 19:22:52 -0500

    Working on iOS.  All buttons functional.  Camera self-view needed.

---
+++ Assets/SmartDecoAppController.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SmartDecoAppController : MonoBehaviour
+{
+
+    public GameObject marketPlacePanel;
+    public GameObject itemViewPanel;
+    public GameObject faceCastPanel;
+    public GameObject devicesPanel;
+    public GameObject accountPanel;
+    public GameObject searchPanel;
+
+    public Button marketPlaceButton;
+    public Button faceCastButton;
+    public Button devicesButton;
+    public Button accountButton;
+    public Button searchButton;
+
+    private List<Button> ButtonList;
+    public Color pressedButtonColor;
+
+
+    void Start()
+    {
+        marketPlacePanel.SetActive(true);
+        itemViewPanel.SetActive(false);
+        faceCastPanel.SetActive(false);
+        devicesPanel.SetActive(false);
+        accountPanel.SetActive(false);
+        searchPanel.SetActive(false);
+
+        marketPlaceButton.onClick.AddListener(pressedMarketPlaceButton);
+        faceCastButton.onClick.AddListener(pressedFaceCastButton);
+        devicesButton.onClick.AddListener(pressedDevicesButton);
+        accountButton.onClick.AddListener(pressedAccountButton);
+        searchButton.onClick.AddListener(pressedSearchButton);
+
+        ButtonList = new List<Button>();
+        ButtonList.Add(marketPlaceButton);
+        ButtonList.Add(faceCastButton);
+        ButtonList.Add(devicesButton);
+        ButtonList.Add(accountButton);
+        ButtonList.Add(searchButton);
+
+        // Start with marketPlaceButton pressed
+        pressedMarketPlaceButton();
+
+    }
+
+
+
+    public void pressedMarketPlaceButton()
+    {
+        HighlightPressedButton(marketPlaceButton);
+        HideAllPanels();
+        marketPlacePanel.SetActive(true);
+    }
+    public void pressedFaceCastButton()
+    {
+        HighlightPressedButton(faceCastButton);
+        HideAllPanels();
+        faceCastPanel.SetActive(true);
+    }
+    public void pressedDevicesButton()
+    {
+        HighlightPressedButton(devicesButton);
+        HideAllPanels();
+        devicesPanel.SetActive(true);
+    }
+    public void pressedAccountButton()
+    {
+        HighlightPressedButton(accountButton);
+        HideAllPanels();
+        accountPanel.SetActive(true);
+    }
+    public void pressedSearchButton()
+    {
+        HighlightPressedButton(searchButton);
+        // HideAllPanels(); // search panel pops up over others
+        searchPanel.SetActive(true);
+    }
+
+    public void HighlightPressedButton(Button pressedButton)
+    {
+        foreach (Button b in ButtonList)
+        {
+            var buttonImageToClear = b.GetComponentInChildren<Image>();
+            if (b == pressedButton)
+            {
+                buttonImageToClear.color = pressedButtonColor;
+            }
+            else
+            {
+                buttonImageToClear.color = Color.white;
+            }
+        }
+    }
+    public void HideAllPanels()
+    {
+        // hide all panels...
+        marketPlacePanel.SetActive(false);
+        faceCastPanel.SetActive(false);
+        devicesPanel.SetActive(false);
+        accountPanel.SetActive(false);
+        searchPanel.SetActive(false);
+
+        itemViewPanel.SetActive(false);
+    }
+}
